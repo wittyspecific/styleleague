1196c4dab69005635cce9466ddd5d2fa334ee254
wittyspecific <105812636+wittyspecific@users.noreply.github.com>
2026-06-12 01:12:18 +0200

    Fix duplicate main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
 import 'package:firebase_core/firebase_core.dart';
+
+import 'app.dart';
 import 'firebase_options.dart';
-import 'features/auth/auth_gate.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,25 +11,5 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const StyleLeagueApp());
-}
-
-class StyleLeagueApp extends StatelessWidget {
-  const StyleLeagueApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'StyleLeague',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const AuthGate(),
-    );
-  }
-}
-
-void main() {
   runApp(const StyleLeagueApp());
 }
